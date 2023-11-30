957520683cdce9aa11109cf12f5c646c3296326d
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2023-11-30 23:56:28 +0000

    Added a loop and a ranged of results since there are too many to display

--- assets/js/museumAPI.js
+++ assets/js/museumAPI.js
@@ -1,21 +1,38 @@
 function displayArtistData() {
     // API setup
     //var artist = $('#search-bar').val().trim();
-    var API = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=vangogh`;
+    var queryURL = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=picasso`;
 
-    fetch(API)
+    fetch(queryURL)
     .then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data);
+
+        // ObjectIDs
+        console.log(data.objectIDs);
+
         // Information for code here
-        var result = data.objectIDs[0];
-        var url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${result}`;
-        fetch(url)
-        .then(function(response) {
-            return response.json();
-        }).then(function(subdata) {
-            console.log(subdata);
+        var result = data.objectIDs;
+
+        // Range for results (Set to 20 as default)
+        var subResults = result.slice(1, 21)
+
+        // Sliced ObjectIDs
+        console.log(subResults)
+
+        subResults.forEach(function(ID) {     
+
+            var url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${ID}`;
+
+            fetch(url)
+            .then(function(response) {
+                return response.json();
+            }).then(function(subData) {
+
+                // Desired Information
+                console.log(subData);
+
+            })        
         })
     })
 }
